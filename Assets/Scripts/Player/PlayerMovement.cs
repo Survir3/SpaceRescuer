9f18d2e3f584560bf174b7c3417bd68343edd4ef
Survir3 <batareyaka@gmail.com>
2023-03-26 04:46:41 +0300

    Add SurvivorRotation_from_Transform

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,6 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speedMovement;
     [SerializeField] private float _speedRotation;
-    [SerializeField] private Transform _test;
 
     private PlayerInput _playerInput;
     private Rigidbody _rigidbody;
@@ -19,19 +18,17 @@ public class PlayerMovement : MonoBehaviour
     private void OnEnable()
     {
         _playerInput.Enable();
-        _playerInput.Player.Move.performed += ctx => OnRotate();
     }
 
     private void OnDisable()
     {
         _playerInput.Disable();
-        _playerInput.Player.Move.performed -= ctx => OnRotate();
     }
 
     private void FixedUpdate()
     {
         Move();
-        OnRotate();
+        Rotate();
     }
 
     private void Move()
@@ -40,15 +37,15 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody.MovePosition(_rigidbody.position+ direction * _speedMovement*Time.deltaTime);
     }
 
-    private void OnRotate()
+    private void Rotate()
     {
-        Quaternion targetRotation = TryGetTargetRotation(_playerInput.Player.Move.ReadValue<float>());
+        Quaternion targetRotation = GetTargetRotation(_playerInput.Player.Move.ReadValue<float>());
         _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, targetRotation, _speedRotation * Time.deltaTime);
     }
 
-    private Quaternion TryGetTargetRotation(float direction)
+    private Quaternion GetTargetRotation(float direction)
     {
-        Vector3 targetPosition=(transform.position + transform.TransformDirection( new Vector3(direction, 0, 0)) - transform.position).normalized;
+        Vector3 targetPosition=transform.TransformDirection( new Vector3(direction, 0, 0));
         return Quaternion.FromToRotation(transform.forward, targetPosition) * _rigidbody.rotation;     
     }
 }
--- Assets/Scripts/SurvivorMovement.cs
+++ Assets/Scripts/SurvivorMovement.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Survivor))]
+[RequireComponent(typeof(Rigidbody))]
 public class SurvivorMovement : MonoBehaviour
 {
     [SerializeField] private float _speedMovement;
+    [SerializeField] private float _distanceToTarget;
+
     private Transform _target;
     private Rigidbody _rigidbody;
 
 
     private void Awake()
     {
-        _rigidbody= GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
@@ -18,6 +20,9 @@ public class SurvivorMovement : MonoBehaviour
         if (_target != null)
         {
             Move();
+            Rotate_transform();
+            // Rotate_MoveRotation();
+           // Rotate_rotation();
         }
     }
 
@@ -28,6 +33,28 @@ public class SurvivorMovement : MonoBehaviour
 
     private void Move()
     {
-        _rigidbody.position= Vector3.MoveTowards(_rigidbody.position,_target.position, _speedMovement * Time.deltaTime);
+        if (Vector3.Distance(_target.position, transform.position) >= _distanceToTarget)
+        {
+            Vector3 direction = _target.position - transform.position.normalized;
+            _rigidbody.position = Vector3.MoveTowards(_rigidbody.position, _target.position, _speedMovement * Time.deltaTime);
+        }
+    }
+
+    private void Rotate_rotation()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(transform.forward, _target.position - transform.position);
+        _rigidbody.rotation= rotation*_rigidbody.rotation;
+    }
+
+    private void Rotate_MoveRotation()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(transform.forward, _target.position - transform.position);
+        _rigidbody.MoveRotation(rotation);
+    }
+
+    private void Rotate_transform()
+    {
+        Quaternion rotation = Quaternion.FromToRotation(transform.forward, _target.position - transform.position);
+        transform.rotation = rotation * transform.rotation;
     }
 }
